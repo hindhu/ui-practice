99e2c86672cfc4ed2ab6bff066c80bb7fd7472fc
hindhu <46604984+hindhu@users.noreply.github.com>
2019-03-05 14:40:00 -0500

    Add files via upload

---
+++ program-1.js
@@ -0,0 +1,10 @@
+function Person(){
+    return(
+        <div>
+            <h1> Hindhu </h1>
+            <p> Surname = Siliveru </p>
+            </div>        
+    );
+}
+ReactDOM.render(<Person />,
+    document.querySelector('#p1'));
